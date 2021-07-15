91b85ce14012c59402a80d52349bb1afd6e5de37
Bala1960 <athreyaOne@gmail.com>
2021-07-15 19:35:40 +0530

    update the data

---
+++ ArrayOne.java
@@ -0,0 +1,17 @@
+// this program is to display the elements of the array
+import java.util.*;
+import java.io.*;
+class ArrayOne
+{
+ 
+public static void main(String args[]) 
+{
+ // initialize the array
+  int ar1[]= new int[] {35,18,8,76,66};
+  for(int i=0;i<ar1.length;i++)
+  {
+       System.out.println(ar1[i]+" ");    
+}      
+ }
+} 
+                 
